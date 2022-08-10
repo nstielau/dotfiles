2ec0be814a6345f625f9b5de80674be28789fadd
Nicholas Stielau <nstielau@Nicholass-MacBook-Pro.local>
2022-08-10 12:38:48 -0700

    Adding hammerspoon

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,97 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+  hs.alert.show("Hello World!")
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "space", function()
+  local chrome = hs.application.find("Chrome")
+  local moveToTab = {"Tab", "Move Tab to New Window"} 
+  chrome:selectMenuItem(moveToTab)
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+
+end)
+
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+  hs.alert.show("Left")
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+  hs.alert.show("Right")
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
+  hs.alert.show("Fullscreen")
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w 
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", function()
+  hs.alert.show("3/4 Screen")
+
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + max.w / 4 
+  f.y = max.y
+  f.w = max.w / 4 * 3
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+hs.alert.show("Config loaded")
